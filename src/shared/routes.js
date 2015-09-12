020c5f443214e669ae9b5568ab7cde06ad0ab803
Jason Blanchard <jas.blanchard@gmail.com>
2015-09-12 08:56:07 -0400

    Added 404 container

--- src/shared/routes.js
+++ src/shared/routes.js
@@ -4,11 +4,13 @@ import RootContainer from './containers/RootContainer';
 import About from './containers/about';
 import ExampleContainer from './containers/ExampleContainer';
 import HelloContainer from './containers/HelloContainer';
+import NoMatch from './containers/NoMatch.js';
 
 export default (
   <Route path="/" component={RootContainer}>
     <Route path="/about" component={About} />
     <Route path="/hello/:name" component={HelloContainer} />
     <IndexRoute component={ExampleContainer} />
+    <Route path="*" component={NoMatch}/>
   </Route>
 );
