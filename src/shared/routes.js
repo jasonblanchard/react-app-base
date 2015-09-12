2730710cc19f01491b081fc5e92fa0b756616c19
Jason Blanchard <jas.blanchard@gmail.com>
2015-09-12 08:56:58 -0400

    Renamed file

--- src/shared/routes.js
+++ src/shared/routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { IndexRoute, Route } from 'react-router';
 import RootContainer from './containers/RootContainer';
-import About from './containers/about';
+import AboutContainer from './containers/AboutContainer';
 import ExampleContainer from './containers/ExampleContainer';
 import HelloContainer from './containers/HelloContainer';
 import NoMatch from './containers/NoMatch.js';
 
 export default (
   <Route path="/" component={RootContainer}>
-    <Route path="/about" component={About} />
+    <Route path="/about" component={AboutContainer} />
     <Route path="/hello/:name" component={HelloContainer} />
     <IndexRoute component={ExampleContainer} />
     <Route path="*" component={NoMatch}/>
