678a1c4c2613d8769becd3611320ece20d17c568
Jason Blanchard <jas.blanchard@gmail.com>
2015-09-17 17:48:18 -0400

    Added title to example container

--- src/shared/containers/ExampleContainer.js
+++ src/shared/containers/ExampleContainer.js
@@ -3,16 +3,19 @@ import { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { actions } from '../actions/actions';
+import { AnnounceableDocumentTitle } from 'react-announceable-document-title';
 import ExampleChild from '../components/ExampleChild';
 
 class ExampleContainer extends Component {
 
   render() {
     return (
-      <div>
-        <h2>Example Connected Component</h2>
-        <ExampleChild {...this.props} />
-      </div>
+      <AnnounceableDocumentTitle title="Example Smart Container">
+        <div>
+          <h2>Example Connected Component</h2>
+          <ExampleChild {...this.props} />
+        </div>
+      </AnnounceableDocumentTitle>
     );
   }
 }
