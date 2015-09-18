f32220cdfc2516ec5ee4eba159ed495392bc8fb7
Jason Blanchard <jas.blanchard@gmail.com>
2015-09-18 10:47:36 -0400

    Updated webpack production config

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -17,7 +17,7 @@ module.exports = {
 
   module: {
     loaders: [
-      { test: /\.jsx?$/, loaders: ['react-hot', 'babel-loader?experimental'], exclude: /node_modules/ }
+      { test: /\.jsx?$/, loaders: ['babel-loader?experimental'], exclude: /node_modules/ }
     ]
   }
 }
